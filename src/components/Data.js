e68a6bff65a4f604da47b1fabcdd580d2c8a74a4
Gabriel Brandão <124525153+Gabriel372@users.noreply.github.com>
2023-09-17 13:05:50 -0300

    Update Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,29 +1,21 @@
 import style from './Data.module.css'
 import {useState,useEffect} from 'react'
 function Data() {
-// const [useDate,setDate] = useState('')
-
 const [useDay,setDay] = useState('')
 const [useMon,setMon] = useState('')
 const [useYear,setYear] = useState('')
 
-
-
-
-useEffect( () => {//useffect monitora cada alteracao do tempo
+useEffect( () => {
 const dayNow = () => {
 let timer = new Date() ;
 let day = timer.getDate() ;
 let mon = timer.getMonth() ;
 let year = timer.getFullYear() ;
-// let dmy = `${day}/${mon}/${year}` //soma hh mm ss
-// setDate(dmy)//clock definido
 setDay(day)
 setMon(mon)
 setYear(year)
 }
-dayNow()//chama a func
-// setInterval(timerNow,1000)//atualizacao a cada s
+dayNow()
 },[])
 
 return<div className={style.divData} >
